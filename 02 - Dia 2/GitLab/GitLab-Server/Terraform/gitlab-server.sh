4412219ad863e24b9d7e413e602a5530b8525616
Christian Ibiri <cibiri@ohgiftcard.com>
2021-06-13 21:32:03 -0300

    modifications to gitlab install script

--- 02 - Dia 2/GitLab/GitLab-Server/Terraform/gitlab-server.sh
+++ 02 - Dia 2/GitLab/GitLab-Server/Terraform/gitlab-server.sh
@@ -4,3 +4,12 @@ curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script
 apt install jq -y
 externalip=$(curl ifconfig.co)
 sudo EXTERNAL_URL="http://$externalip" apt-get install gitlab-ce
+
+# Install Docker
+apt-get install docker.io -y
+
+# Install GitLab Runner
+curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb"
+dpkg -i gitlab-runner_amd64.deb
+sudo usermod -aG docker gitlab-runner
+service gitlab-runner restart
