9dffe7801111f2d96b9819cfd815e46069f03195
Christian Ibiri <cibiri@ohgiftcard.com>
2021-06-13 21:27:56 -0300

    added ACI for SonarQube

---
+++ 02 - Dia 2/GitLab/Sast - SonarQube/Terraform/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "SonarQube"
+  location = "eastus"
+}
+
+resource "azurerm_container_group" "example" {
+  name                = "SonarQube-Server"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  ip_address_type     = "public"
+  dns_name_label      = "cibiri-sonar"
+  os_type             = "Linux"
+
+  container {
+    name   = "sonarqube"
+    image  = "sonarqube"
+    cpu    = "2"
+    memory = "3.5"
+
+    ports {
+      port     = 9000
+      protocol = "TCP"
+    }
+  }
+}
