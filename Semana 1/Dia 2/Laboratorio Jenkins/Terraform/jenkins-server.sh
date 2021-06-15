3c59c7a2dc34203d3d15e5b315847514ee6e483b
Christian Ibiri <cibiri@ohgiftcard.com>
2021-06-15 01:24:50 -0300

    added jenkins

---
+++ Semana 1/Dia 2/Laboratorio Jenkins/Terraform/jenkins-server.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# Install Jenkins
+sudo apt update -y
+sudo apt install openjdk-8-jdk -y
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins -y
+
+# Install Hugo
+wget https://github.com/gohugoio/hugo/releases/download/v0.83.1/hugo_0.83.1_Linux-64bit.deb
+dpkg -i hugo_0.83.1_Linux-64bit.deb
+
+
