13846eddca26647f94183f7163f0cfb66e3b8080
Christian Ibiri <cibiri@ohgiftcard.com>
2021-06-23 00:25:51 -0300

    added gitlab init password

--- Semana 1/Dia 2/Laboratorio GitLab/Terraform/gitlab-server.sh
+++ Semana 1/Dia 2/Laboratorio GitLab/Terraform/gitlab-server.sh
@@ -3,7 +3,7 @@
 curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 apt install jq -y
 externalip=$(curl ifconfig.co)
-sudo EXTERNAL_URL="http://$externalip" apt-get install gitlab-ce
+sudo EXTERNAL_URL="http://$externalip" GITLAB_ROOT_PASSWORD=Password1234! apt-get install gitlab-ce
 
 # Install Docker
 apt-get install docker.io -y
