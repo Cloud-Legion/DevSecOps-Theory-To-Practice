965cd484b584e1415666874b15b4c4bfc8f55993
Christian Ibiri <cibiri@ohgiftcard.com>
2021-07-12 09:26:03 -0300

    port modified

--- Semana 2/Dia 5/Inspect/Terraform/main.tf
+++ Semana 2/Dia 5/Inspect/Terraform/main.tf
@@ -60,13 +60,13 @@ resource "azurerm_network_security_group" "myterraformnsg" {
     resource_group_name = azurerm_resource_group.myterraformgroup.name
 
     security_rule {
-        name                       = "All"
+        name                       = "SSH"
         priority                   = 1001
         direction                  = "Inbound"
         access                     = "Allow"
         protocol                   = "Tcp"
         source_port_range          = "*"
-        destination_port_range     = "*"
+        destination_port_range     = "22"
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
