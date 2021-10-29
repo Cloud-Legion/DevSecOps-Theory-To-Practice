1e98d15139e42a6acff3fcb8cdd98ac9fba5cb3b
Christian Ibiri <cibiri@ohgiftcard.com>
2021-10-29 18:08:31 -0300

    latest

--- Dia 4/Laboratorio Docker/Terraform/docker-server.sh
+++ Dia 4/Laboratorio Docker/Terraform/docker-server.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
+
 # Install Docker
 apt-get update 
 apt-get install docker.io -y
 
+# Install GitLab Runner
+curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb"
+dpkg -i gitlab-runner_amd64.deb
+sudo usermod -aG docker gitlab-runner
+service gitlab-runner restart
